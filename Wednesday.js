cc177d5112963d49ace1f099045cfa79524c967a
Van Ha <hav206@yahoo.com>
2018-03-21 12:09:36 -0700

    Commit 4- Fizz Buzz answered

--- Wednesday.js
+++ Wednesday.js
@@ -1,11 +1,23 @@
 
-var temp = ["..Human..Why are taking pictures of me", "...The catnip made me doit...",
-"..Why does the red dot always get awasy.."];
-for (i = 0; i <= 20; i++){
-	console.log("Love me, pet me!")
-	if(i % 2 == 0)
-	{
-		console.log(temp[Math.floor(temp.length * Math.random())]);
-	}
+//var temp = ["..Human..Why are taking pictures of me", "...The catnip made me doit...",
+//"..Why does the red dot always get awasy.."];
+//for (i = 0; i <= 20; i++){
+	//console.log("Love me, pet me!")
+	//if(i % 2 == 0)
+	//{
+		//console.log(temp[Math.floor(temp.length * Math.random())]);
+	//}
 	
+//}
+
+for(var i = 1; i < 100; i++){
+	if(i % 3 == 0){
+		console.log("Fizz");
+	}
+	if(i % 5 == 0){
+		console.log("Buzz");
+	}
+	if(i % 3 === 0 && i % 5 === 0){
+		console.log("FizzBuzz");
+	}
 }
