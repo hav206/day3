33d46b60f682da3261b5d9ba65ba223f23874d2f
Van Ha <hav206@yahoo.com>
2018-03-21 12:02:24 -0700

    Commit 2- Get Even answered

--- Wednesday.js
+++ Wednesday.js
@@ -1,3 +1,11 @@
-for (i = 0; i <= 200; i = i+ 2){
-	console.log(i)
+
+var temp = ["..Human..Why are taking pictures of me", "...The catnip made me doit...",
+"..Why does the red dot always get awasy.."];
+for (i = 0; i <= 20; i++){
+	console.log("Love me, pet me!")
+	if(i % 2 == 0)
+	{
+		console.log(temp[Math.floor(temp.length * Math.random())]);
+	}
+	
 }
